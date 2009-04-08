e49bfc79bc8c40e250288371791b420570e40e5e
Moritz Lenz <moritz@faui2k3.org>
2009-04-08 17:19:25 +0200

    test transposed solving

--- test.cpp
+++ test.cpp
@@ -4,19 +4,39 @@
 #include <Eigen/Sparse>
 using namespace std;
 
-USING_PART_OF_NAMESPACE_EIGEN
+using namespace Eigen;
 
 typedef double num;
 typedef complex<num> cnum;
 typedef Eigen::SparseMatrix< cnum , Eigen::RowMajor> esm;
+typedef Eigen::RandomSetter< esm > ers;
+typedef Eigen::SparseLU<esm, Eigen::SuperLU> eslu;
 
 int main(int argc, char** argv) {
-	MatrixXcd foo(3, 3);
-	foo(0, 2) = cnum(2, 3);
-	cout << foo << endl;
+    esm a(2, 2);
+    {
+        ers s(a);
+        s(0, 0) = cnum(2, 0);
+        s(0, 1) = cnum(1, 1);
+        s(1, 0) = cnum(2, 1);
+    }
+    {
+        eslu s(a);
+        VectorXcd rhs(2);
+        VectorXcd solution(2);
+        rhs(0) = cnum(1, 0);
+        rhs(1) = cnum(2, 0);
 
-	foo = foo.adjoint().eval();
-	cout << "adjoint: \n";
+        s.solve(rhs, &solution);
+        cout << solution << "\n\n";
+        cout << a * solution << "\n\n";
 
-	cout << foo << endl;
+        s.solve(rhs, &solution, SvTranspose);
+        cout << solution << "\n\n";
+        cout << a.transpose() * solution << "\n\n";
+
+        s.solve(rhs, &solution, SvAdjoint);
+        cout << solution << "\n\n";
+        cout << a.adjoint() * solution << "\n\n";
+    }
 }
