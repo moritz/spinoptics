c045172ee289132bfc38a2a0cf83c2f4cccf811a
Moritz Lenz <mlenz@wvbh074.physik.uni-wuerzburg.de>
2009-08-26 10:46:29 +0200

    very basic plotting script

---
+++ plot.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use lib 'lib';
+use ReadMatrix qw(real_matrix);
+use Data::Dumper;
+
+my $d = shift(@ARGV) // die "Usage: $0 <datadir>";
+
+my @files = glob "data/$d/*.dat" or die "No files in `data/$d/*'";
+
+my @d;
+for my $fn (@files) {
+    $fn =~ /phi(\d\d)\.dat/ or die "can't work with file name `$fn'";
+    my $phi = $1;
+    my $m = tpq_for_file($fn);
+    print $phi, " ", $m->[5][5], $/;
+}
+
+
+sub tpq_for_file {
+    my $fn = shift;
+    open my $handle, '<', $fn or die "Can't open file `$fn' for reading: $!";
+    my $m;
+    while (<$handle>) {
+        if (/^final tpq/) {
+            $m = real_matrix($_);
+            last;
+        }
+    }
+    close $handle or warn $!;
+    return $m;
+}
+
+# vim: sw=4 ts=4 expandtab
