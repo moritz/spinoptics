480a8c1bdb392a3cd58a6ccb1ea4c09d17481e14
Moritz Lenz <mlenz@wvbh074.physik.uni-wuerzburg.de>
2009-10-05 16:07:56 +0200

    [run.pl] distribute parallel runs over the network, crudely

--- run.pl
+++ run.pl
@@ -2,6 +2,10 @@
 use strict;
 use warnings;
 use Parallel::ForkManager;
+use Data::Dumper;
+
+my @hosts = glob "wvbh07{1,2,3,5,6,7,8}";
+my $parallel_jobs = @hosts;
 
 my $dir = 'data/' . int(rand() * 10_000);
 die "bad luck" if -e $dir;
@@ -12,23 +16,31 @@ print "Writing data to `$dir'\n";
 
 my $b = 0;
 
-my $pm = Parallel::ForkManager->new(2);
+my $pm = Parallel::ForkManager->new($parallel_jobs);
 
+my $count = -1;
 for my $phi (0..90) {
+    $count++;
     my $pid = $pm->start and next;
     my $angle = $phi / 180 * 3.14159;
-    print "Phi = ", $phi, " degrees\n";
+    my $host = $hosts[$count % @hosts];
+    print "($host) Phi = ", $phi, " degrees\n";
         my $fn = sprintf "%s/bz%+.2f,phi%02d.dat", $dir, $b, $phi;
-        system('./cppspin',
-                -b => $b,
-                -e => -2.5,
-                -o => $fn,
-                -r => -0.5,
-                -p => $angle,
-                '-q',
-                -n => 19,
-            ) == 0
-            or die "can't run ./cppsin: $?";
+        my @args = (
+            -b => $b,
+            -e => -2.5,
+            -o => $fn,
+            -r => -0.5,
+            -p => $angle,
+            '-q',
+            -n => 19,
+        );
+        my $ret = system('ssh', '-x', $host, './run.sh', @args);
+        if ($ret != 0) {
+            warn "can't run ssh $host run.sh: $?, $!\n";
+            warn "re-running it locally...\n";
+            system './cppspin', @args;
+        }
     $pm->finish;
 }
 
