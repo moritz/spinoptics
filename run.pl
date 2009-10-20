930586a1e91afd6983b4dadf46dd6f46c4f1b925
Moritz Lenz <mlenz@wvbh074.physik.uni-wuerzburg.de>
2009-10-20 16:06:13 +0200

    [run.pl] more hosts, wait for the processes to finish

--- run.pl
+++ run.pl
@@ -4,7 +4,7 @@ use warnings;
 use Parallel::ForkManager;
 use Data::Dumper;
 
-my @hosts = glob "wvbh07{2,3,3,5,6,7,8} wthp10{4,4,5,5,6,6}";
+my @hosts = glob "wvbh07{0,2,3,3,5,6,7,8} wvbh06{6,9} wthp00{6,9} wthp010 wthp10{4,4,5,5,6,6}";
 my $parallel_jobs = @hosts;
 my $revoke;
 $revoke = 1 if $ARGV[0] && $ARGV[0] eq 'revoke';
@@ -28,20 +28,21 @@ for my $phi (0..90) {
     my $pid = $pm->start and next;
     my $host = $hosts[$count % @hosts];
     if ($revoke) {
-        system 'ssh', '-X', $host, 'killall', 'cppspin';
+        system 'ssh', '-x', $host, 'killall', 'cppspin';
     } else {
         my $fn = sprintf "%s/bz%+.2f,phi%02d.dat", $dir, $b, $phi;
         my @args = (
             -b => $b,
-            -e => 2.0,
+            -e => 0.02,
             -o => $fn,
-            -r => 0.02,
+            -r => 0.05,
             -p => $phi,
             '-q',
             -n => 19,
         );
+        print "START: ($host) Phi = ", $phi, " degrees\n";
         my $ret = system('ssh', '-x', $host, './run.sh', @args);
-        print "($host) Phi = ", $phi, " degrees\n";
+        print "END:   ($host) Phi = ", $phi, " degrees\n";
         if ($ret != 0) {
             warn "can't run ssh $host run.sh: $?, $!\n";
             warn "re-running it locally...\n";
@@ -50,6 +51,7 @@ for my $phi (0..90) {
     }
     $pm->finish;
 }
+$pm->wait_all_children;
 
 print "finished run `$dir'\n";
 
