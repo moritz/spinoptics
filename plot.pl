3dbf655872de78e20b74ae3adc8a8c8e1aa2e0cf
Moritz Lenz <mlenz@wvbh074.physik.uni-wuerzburg.de>
2009-11-02 17:20:13 +0100

    [plot.pl] allow plotting of multiple datasets at once

--- plot.pl
+++ plot.pl
@@ -4,17 +4,40 @@ use warnings;
 use lib 'lib';
 use ReadMatrix qw(real_matrix);
 use Data::Dumper;
+use 5.010;
 
-my $d = shift(@ARGV) // die "Usage: $0 <datadir>";
-
-my @files = glob "data/$d/*.dat" or die "No files in `data/$d/*'";
-
+my @x;
 my @d;
-for my $fn (@files) {
-    $fn =~ /phi(\d\d)\.dat/ or die "can't work with file name `$fn'";
-    my $phi = $1;
-    my $m = tpq_for_file($fn);
-    print $phi, " ", ($m->[0][2] + $m->[1][2]), $/;
+my $isfirst = 1;
+for my $d (@ARGV) {
+    my @files = glob "data/$d/*.dat" or die "No files in `data/$d/*'";
+
+    my $i = 0;
+    for my $fn (@files) {
+        $fn =~ /alpha([\d.]+)\.dat/ or die "can't work with file name `$fn'";
+        my $alpha = $1;
+        my $m = tpq_for_file($fn);
+        if ($isfirst) {
+            push @x, $alpha;
+        } else {
+            if (abs($x[$i]-$alpha) > 1e5) {
+                die "Can't merge datasets with different X axes\n"
+                    . "  for the ${i}th set I expected $x[$i] and got $alpha";
+            }
+        }
+        push @{$d[$i]}, $m->[0][2] + $m->[1][2];
+    } continue {
+        $i++;
+    }
+    $isfirst = 0;
+}
+use Data::Dumper;
+#print Dumper \@x;
+#print Dumper \@d;
+
+for my $i (0..$#x) {
+    say $x[$i], ' ', join(' ', @{$d[$i]});
+
 }
 
 
